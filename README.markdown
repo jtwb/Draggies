dc4ce604eeebae621bf9aee57b282c692d0ba0a6
Jason Benterou <jason.benterou@gmail.com>
2010-02-28 21:47:11 -0800

    prepare for github hosting

---
+++ README.markdown
@@ -0,0 +1,28 @@
+Draggies is a demo application built on node.js, faye bayeux server and jQuery UI.
+
+# Application
+
+Draggies is a real-time collaborative web application involving draggable boxes with letters, sort of like a child's alphabet blocks.  Draggies uses the [faye bayeux server](http://github.com/jcoglan/faye) to broadcast all user-made changes to all other users in real time.  Application state is recorded and new clients are brought up-to-date.
+
+# Known Issues
+
+- Rendering issues in Opera
+- Text input is limited to A-Z and 0-9
+
+# Usage
+
+First you'll need [node.js](http://nodejs.org/) installed.  The current version of this application uses node.js [version 0.1.29](http://github.com/ry/node/tree/v0.1.29).
+
+To checkout Draggies and its dependencies:
+
+    git checkout git://github.com/jtwb/Draggies.git
+    git submodule init
+    git submodule update
+
+To run Draggies:
+
+    cd Draggies/
+    sudo node draggieserver.js
+
+Application will be served on `/index.html`
+
---
+++ draggieserver.js
@@ -0,0 +1,68 @@
+var sys = require('sys'),
+    http = require('http'),
+    fs = require('fs'),
+    json = require('./json'),
+    faye = require('./faye');
+
+var comet = new faye.NodeAdapter({mount: '/fayeclient', timeout: 45}),
+    client = comet.getClient();
+
+// setup sync client; TODO move into a separate module
+var state = {};
+client.subscribe('/general', function(message) {
+// TODO discard malformed messages
+   if (!message.client || !message.type) return;
+   sys.puts('sync noticed message from client ' + message.client);
+   sys.puts('message type == ' + message.type);
+   switch (message.type) {
+      case 'place' :
+         state[message.el] || (state[message.el] = {});
+         process.mixin(state[message.el], {
+            x: message.x,
+            y: message.y
+         });
+         break;
+      case 'text' :
+         process.mixin(state[message.el], {
+            text: message.text,
+         });
+         break;
+      case 'delete' :
+         delete state[message.el];
+         break;
+   }
+   sys.puts("state: ");
+   sys.puts(json.stringify(state));
+});
+
+var port = 80;
+
+sys.puts('Listening on ' + port);
+
+http.createServer(function(req, resp) {
+   sys.puts(req.method + ' ' + req.url);
+   if (comet.call(req, resp)) {
+      sys.puts('** Handled by faye');
+      return;
+   }
+
+  var path = (req.url === '/') ? '/index.html' : req.url;
+  if (path === '/sync') {
+      sys.puts('** Handled by syncserver');
+      resp.sendHeader(200, {'Content-Type': 'text/html'});
+      resp.write(json.stringify(state));
+      resp.close();
+      return;
+  }
+  sys.puts('** Handled by file server');
+  fs.readFile('./static/' + path).addCallback(function(content) {
+    resp.sendHeader(200, {'Content-Type': 'text/html'});
+    resp.write(content);
+    resp.close();
+  }).addErrback(function(){
+    resp.sendHeader(404, {'Content-Type': 'text/html'});
+    resp.write('<html><head><title>404 not found</title></head><body>404 Not Found</body></html>');
+    resp.close();
+  });
+}).listen(port);
+
